1d0e3e286966976d243086127be368c0bbcf4ec1
Jinji55 <yairtadmor@gmail.com>
2024-12-28 11:30:20 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: false
-  },
-  typescript: {
-    ignoreBuildErrors: true
   }
 }
 
