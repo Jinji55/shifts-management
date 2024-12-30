c8b43313ad0ecca7cbd6b749667f303040859e7f
Jinji55 <yairtadmor@gmail.com>
2024-12-30 10:28:47 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import ShiftsTable from '../components/ShiftsTable';
+import ShiftManagement from '../components/ShiftManagement';
 
 export default function Home() {
   return (
     <main className="min-h-screen p-8 bg-gray-50">
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold mb-8 text-center">מערכת ניהול משמרות</h1>
-        <ShiftsTable />
+        <ShiftManagement />
+        <div className="mt-8">
+          <ShiftsTable />
+        </div>
       </div>
     </main>
   );
